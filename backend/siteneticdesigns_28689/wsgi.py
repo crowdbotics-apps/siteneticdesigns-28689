d04cc9b3aff6d36d02aa13b9ec959988a7eafde3
Crowdbotics <team@crowdbotics.com>
2021-07-10 14:26:28 +0000

    Initial commit.

---
+++ backend/siteneticdesigns_28689/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for siteneticdesigns_28689 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'siteneticdesigns_28689.settings')
+
+application = get_wsgi_application()
